de1542131a65041536881358b1b2c86fca3aa6ce
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-08-26 18:10:43 +0700

    remove unused import and component from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
@@ -17,9 +16,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={router} />
       <ReactQueryDevtools/>
     </QueryClientProvider>
   </StrictMode>,
