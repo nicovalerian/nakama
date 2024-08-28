805877eddb248f971653529254d662e3bf4ae90b
Nico <89244947+nicovalerian@users.noreply.github.com>
2024-08-28 17:38:38 +0700

    update HomePage with product images

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function HomePage({ products }) {
   return (
@@ -24,7 +25,7 @@ function HomePage({ products }) {
 
           <ul className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3">
             <li>
-              <a href="#" className="group relative block">
+              <Link to="/shop" className="group relative block">
                 <img
                   src="https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                   alt=""
@@ -40,11 +41,11 @@ function HomePage({ products }) {
                     Shop Now
                   </span>
                 </div>
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a href="#" className="group relative block">
+              <Link to="/shop" className="group relative block">
                 <img
                   src="https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                   alt=""
@@ -60,11 +61,11 @@ function HomePage({ products }) {
                     Shop Now
                   </span>
                 </div>
-              </a>
+              </Link>
             </li>
 
             <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-              <a href="#" className="group relative block">
+              <Link to="/shop" className="group relative block">
                 <img
                   src="https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80"
                   alt=""
@@ -80,7 +81,7 @@ function HomePage({ products }) {
                     Shop Now
                   </span>
                 </div>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
@@ -146,114 +147,42 @@ function HomePage({ products }) {
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
           <header className="text-center">
             <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-             Exclusive <span className="text-nakamaRed">“Kogane”</span> Collection
+              Exclusive <span className="text-nakamaRed">“Kogane”</span>{" "}
+              Collection
             </h2>
 
             <p className="mx-auto mt-4 max-w-md text-gray-500">
-            Step into a world where tradition meets sophistication. The Kogane Collection is a carefully curated selection of rare, hand-crafted Japanese treasures.
+              Step into a world where tradition meets sophistication. The Kogane
+              Collection is a carefully curated selection of rare, hand-crafted
+              Japanese treasures.
             </p>
           </header>
 
           <ul className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-            <li>
-              <a href="#" className="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-                />
+            {products.slice(0, 4).map((product) => (
+              <li key={product.id}>
+                <a href="/shop" className="group block overflow-hidden">
+                  <img
+                    src={product?.image || ""}
+                    alt=""
+                    className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  />
 
-                <div className="relative bg-white pt-3">
-                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                    Basic Tee
-                  </h3>
+                  <div className="relative bg-white pt-3">
+                    <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                      {product.title}
+                    </h3>
 
-                  <p className="mt-2">
-                    <span className="sr-only"> Regular Price </span>
-
-                    <span className="tracking-wider text-gray-900">
-                      {" "}
-                      £24.00 GBP{" "}
-                    </span>
-                  </p>
-                </div>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" className="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-                />
-
-                <div className="relative bg-white pt-3">
-                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                    Basic Tee
-                  </h3>
-
-                  <p className="mt-2">
-                    <span className="sr-only"> Regular Price </span>
-
-                    <span className="tracking-wider text-gray-900">
-                      {" "}
-                      £24.00 GBP{" "}
-                    </span>
-                  </p>
-                </div>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" className="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-                />
-
-                <div className="relative bg-white pt-3">
-                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                    Basic Tee
-                  </h3>
-
-                  <p className="mt-2">
-                    <span className="sr-only"> Regular Price </span>
-
-                    <span className="tracking-wider text-gray-900">
-                      {" "}
-                      £24.00 GBP{" "}
-                    </span>
-                  </p>
-                </div>
-              </a>
-            </li>
-
-            <li>
-              <a href="#" className="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-                />
-
-                <div className="relative bg-white pt-3">
-                  <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                    Basic Tee
-                  </h3>
-
-                  <p className="mt-2">
-                    <span className="sr-only"> Regular Price </span>
-
-                    <span className="tracking-wider text-gray-900">
-                      {" "}
-                      £24.00 GBP{" "}
-                    </span>
-                  </p>
-                </div>
-              </a>
-            </li>
+                    <p className="mt-2">
+                      <span className="sr-only"> Regular Price </span>
+                      <span className="tracking-wider text-gray-900">
+                        ${product.price}
+                      </span>
+                    </p>
+                  </div>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </section>
